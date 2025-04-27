71b82c0526e2234c9e97ea737e62830893d0daf2
AssafAbadi <asaf022@gmail.com>
2025-04-28 02:25:50 +0300

    Create AsyncConfig.java

---
+++ src/main/java/com/jobqueue/AsyncConfig.java
@@ -0,0 +1,11 @@
+package com.jobqueue;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync  
+public class AsyncConfig {// Enable asynchronous method execution
+    
+  
+}
