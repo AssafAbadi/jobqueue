dfe31f344b1cb5981812d8c51a821379a45a1eeb
AssafAbadi <asaf022@gmail.com>
2025-04-28 02:26:36 +0300

    commit

---
+++ src/main/java/com/jobqueue/JobRepository.java
@@ -0,0 +1,10 @@
+package com.jobqueue;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Integer> {
+    Optional<Job> getJobByName(String name); 
+
+}
